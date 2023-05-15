fa36d34d4264f91efda7766151b3aa9d72f5e51d
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-05-15 16:34:54 +0500

    img create

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/ReactP/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- vite-project/src/Components/ReactP/Navbar.jsx
+++ vite-project/src/Components/ReactP/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className='container'>
+      <div className='hero'>
+        <div className='hero__left'>
+          <h1 className='hero__left--title'>
+            __theBee are 80 <span className='hero__left--leni'>Modern</span>
+            Townhomes
+          </h1>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
